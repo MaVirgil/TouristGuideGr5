4b2591c69f35cad3015cd14f67f4764f769fe2d9
WiiTee <somewhereinnowhere@live.dk>
2025-10-09 13:56:42 +0200

    Updated AttractionExistException method to throw correct message

--- src/main/java/com/example/touristguide/exceptions/AttractionExistException.java
+++ src/main/java/com/example/touristguide/exceptions/AttractionExistException.java
@@ -3,7 +3,7 @@ package com.example.touristguide.exceptions;
 import com.example.touristguide.exceptions.service.AttractionServiceException;
 
 public class AttractionExistException extends AttractionServiceException {
-    public AttractionExistException(String message) {
-        super(message);
+    public AttractionExistException(String name) {
+        super("Attraction with name: '" + name + "' already exists");
     }
 }
