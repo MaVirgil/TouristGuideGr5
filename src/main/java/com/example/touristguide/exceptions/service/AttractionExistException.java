aea872bdc6391da890298622d441b74cfd475656
WiiTee <somewhereinnowhere@live.dk>
2025-10-09 13:52:12 +0200

    Added AttractionExistException class

---
+++ src/main/java/com/example/touristguide/exceptions/service/AttractionExistException.java
@@ -0,0 +1,7 @@
+package com.example.touristguide.exceptions.service;
+
+public class AttractionExistException extends RuntimeException {
+    public AttractionExistException(String message) {
+        super(message);
+    }
+}
